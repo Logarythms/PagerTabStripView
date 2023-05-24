a6e750d0659a96f5256053127ac98ddae81b9d46
camillemainz <camille@mainz.me>
2023-05-24 22:39:20 +0200

    fixes xcode 14.3

--- Sources/NavigationBarStyles/IndicatorBarView.swift
+++ Sources/NavigationBarStyles/IndicatorBarView.swift
@@ -10,26 +10,26 @@ import SwiftUI
 
 internal struct IndicatorBarView<Indicator: View>: View {
     @EnvironmentObject private var dataStore: DataStore
-
+    
     @ViewBuilder var indicator: Indicator
-
+    
     var body: some View {
         HStack {
             let totalItemWidth = (settings.width - (style.tabItemSpacing * CGFloat(dataStore.itemsCount - 1)))
             let navBarItemWidth = totalItemWidth / CGFloat(dataStore.itemsCount)
-            if let width = navBarItemWidth, width > 0, width <= settings.width {
-                let x = -settings.contentOffset / CGFloat(dataStore.itemsCount) + width / 2
-
+            if navBarItemWidth > 0, navBarItemWidth <= settings.width {
+                let x = -settings.contentOffset / CGFloat(dataStore.itemsCount) + navBarItemWidth / 2
+                
                 indicator
                     .foregroundColor(style.indicatorBarColor)
                     .animation(.default)
-                    .frame(width: width)
+                    .frame(width: navBarItemWidth)
                     .position(x: x, y: 0)
             }
         }
         .frame(height: style.indicatorBarHeight)
     }
-
+    
     @Environment(\.pagerStyle) var style: PagerStyle
     @EnvironmentObject private var settings: PagerSettings
 }
